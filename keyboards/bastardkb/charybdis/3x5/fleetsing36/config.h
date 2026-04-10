c9d33ee79a9b226479ca8e382917c42eac505e56
fleetsing <thewayoftheatom@gmail.com>
2026-04-10 18:29:58 +0300

    Created side agnostic config

--- keyboards/bastardkb/charybdis/3x5/fleetsing36/config.h
+++ keyboards/bastardkb/charybdis/3x5/fleetsing36/config.h
@@ -2,4 +2,10 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
 
+/*
+ * Persist handedness per half so physical left/right stay stable even when USB
+ * is plugged into the opposite side.
+ */
+#define EE_HANDS
+
 #define POINTING_DEVICE_CS_PIN F0
